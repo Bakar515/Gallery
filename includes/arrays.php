7554c5949339550fcf1c08372a3047970738f551
Abubakar Rizwan <99861285+Bakar515@users.noreply.github.com>
2022-09-29 12:45:30 +0500

    array file add

---
+++ includes/arrays.php
@@ -0,0 +1,27 @@
+<?php
+    
+    $navItems = array(
+                        array(
+                            slug => "index.php",
+                            title => "All"
+                        ),
+                        array(slug => "camera.php",
+                        title => "Camera"
+                        ),
+                        array(slug => "headphone.php",
+                        title => "Headphone"
+                        ),
+                        array(
+                            slug => "phone.php",
+                            title => "Phone"
+                        ),
+                        array(
+                            slug => "shoe.php",
+                            title => "Shoe"
+                        ),
+                        array(
+                            slug => "watch.php",
+                            title => "Watch"
+                        ),
+                    );
+?>
