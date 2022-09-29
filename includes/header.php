0a63fcb7aedd12fccce2022185cad52482c2de46
Abubakar Rizwan <99861285+Bakar515@users.noreply.github.com>
2022-09-29 12:55:04 +0500

    change in nav.php

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,7 @@
+<?php
+    include('includes/arrays.php');
+?>
+
 <!DOCTYPE html>
 
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,13 @@
-<button id="div1show" class="btn btn-dark">All</button>
+<?php
+    foreach ($navItems as $item){
+        echo "<button href=\"$item[slug]\">$item[title]</button>";
+    }
+    
+?>
+
+<!-- <button id="div1show" class="btn btn-dark">All</button>
 <button id="div2show" class="btn btn-dark">Camera</button>
 <button id="div3show" class="btn btn-dark">HeadPhone</button>
 <button id="div4show" class="btn btn-dark">Phone</button>
 <button id="div5show" class="btn btn-dark">Shoe</button>
-<button id="div6show" class="btn btn-dark">Watch</button>
+<button id="div6show" class="btn btn-dark">Watch</button> -->
