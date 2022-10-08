c5bfbfb5e09cfa10af014d460543fbf7350d02bd
Abubakar Rizwan <99861285+Bakar515@users.noreply.github.com>
2022-10-08 15:01:46 +0500

    Update dish.php

--- includes/dish.php
+++ includes/dish.php
@@ -1,8 +1,15 @@
 <?php
     define("TITLE", "MENU | Bakar Gallery App");
     include('includes/header.php');
+
+    //Learn more about preg_replace in php manual.
+
+    function strip_bad_chars($input){
+        $output = preg_replace( "/[^a-zA-z0-9_-]/", "", $input );
+        return $output;
+    }
     if(isset($_GET['item'])){
-        $menuItem = $_GET['item'];
+        $menuItem = strip_bad_chars( $_GET['item'] );
         $dish = $menuItems[$menuItem]; 
     }
 ?>
